c5d05bb4d75c8e9c745104fd601c9874b906ab01
Josu Hernandez <josu.hernandez92@gmail.com>
2022-03-24 17:27:56 +0100

    small changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,6 @@ import styled from 'styled-components'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab, faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
-import { faEnvelope, faMobileAlt } from '@fortawesome/free-solid-svg-icons'
-import { faInstagram } from '@fortawesome/free-brands-svg-icons'
 library.add(fab)
 
 const ContactSection = () => {
